21d245d45ab3f6d4d8577ddef9d00f3e34e2c71b
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2024-03-12 11:01:03 +0100

    dindi

---
+++ SlutProj/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(700, 900, "Platformer");
+Raylib.SetTargetFPS(60);
+
+string scene = "start";
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+
+    if (scene == "start")
+    {
+        Raylib.ClearBackground(Color.LightGray);
+
+        Raylib.DrawText("To Move:", 200, 480, 60, Color.DarkGray);
+        Raylib.DrawText("A", 240, 560, 80, Color.Black);
+        Raylib.DrawText("D", 400, 560, 80, Color.Black);
+
+        Raylib.DrawText("To Fly:", 240, 700, 60, Color.DarkGray);
+        Raylib.DrawText("SPACE", 220, 780, 80, Color.Black);
+    }
+
+    Raylib.EndDrawing();
+}
